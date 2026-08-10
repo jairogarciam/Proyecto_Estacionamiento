5dcd9def47cb9f38d557c4bbaf0f02c258ed58e8
whoischrss <whoislaforteza@gmail.com>
2026-08-10 17:30:21 -0600

    Correccion

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json()); // Permite recibir JSON en los req.body de Postman
-import authRoutes from './modules/auth/auth.routes';
 // Ruta base de prueba
 app.get('/', (req, res) => {
     res.json({ message: 'API del Sistema de Estacionamiento funcionando correctamente 🚗' });
